22f3cb50cce9a65b2dba89c0e77e2885fecc3e29
Rick <derickharon6@gmail.com>
2025-05-22 22:09:55 +0300

    backend updates

--- routes/api.php
+++ routes/api.php
@@ -14,10 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
-
 Route::group(['prefix' => 'v1'], function (){
     Route::prefix('auth')->group(base_path('routes/modules/auth.php'));
     Route::prefix('user')->group(base_path('routes/modules/user.php'));
